75c2abf142319a2d92a8a7f429f90e23e151996f
Sivanand-M <Sivanand.M.Prabhu@gds.ey.com>
2025-12-01 10:17:45 +0530

    Add project files.

---
+++ LogicLibrary/Movie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLogicLibrary
+{
+    public enum SeatType
+    {
+        Recliner,
+        Classic,
+        Premium
+    }
+    public class Movie
+    {
+        public void BookTicket(string moviename)
+        {
+            Console.WriteLine($"Booking for {moviename}");
+        }
+
+        public void BookTicket(string moviename, int cnt)
+        {
+            Console.WriteLine($"Booking for {moviename} , {cnt} tickets");
+        }
+
+        public double BookTicket(string moviename, int cnt, SeatType seatType)
+        {
+            float price = 0;
+            double totalAmt = 0;
+
+
+            switch (seatType)
+            {
+                case SeatType.Recliner:
+                    price = 1000;
+                    totalAmt = price * cnt;
+                    break;
+                case SeatType.Classic:
+                    price = 500;
+                    totalAmt = price * cnt;
+                    break;
+                case SeatType.Premium:
+                    price = 700;
+                    totalAmt = price * cnt;
+                    break;
+                default:
+                    break;
+            }
+
+            return totalAmt;
+        }
+
+    }
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieLogicLibrary;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MovieInvoke(); -----> Movie Method Invoke
+            //ArraySort(arr); -----> Array Sort Method Invoke
+            //ArrayCopy(arr); -----> Array Copy Method Invoke
+            //ArrayContains(); -----> Array Contains Method Invoke
+            int[] arr = new int[] { 5, 3, 8, 1, 4 };
+
+        }
+
+        private static void ArrayContains()
+        {
+            string[] str = new string[] { "Apple", "Orange", "Banana", "Grapes" };
+
+            if (str.Contains("Banana"))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        private static void ArrayCopy(int[] arr)
+        {
+            int[] destArr = new int[5];
+            Array.Copy(arr, destArr, 5);
+
+            Console.WriteLine("Destination Array : ");
+            foreach (var item in destArr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        private static void ArraySort(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nSorted Array : ");
+            Array.Sort(arr);
+
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        private static void MovieInvoke()
+        {
+            Movie m = new Movie();
+            double amt = m.BookTicket("Avatar", 3, SeatType.Recliner);
+            Console.WriteLine(amt);
+        }
+    }
+}
